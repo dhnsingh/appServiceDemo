df22e8123b88fae45ba9a0bf75e1ea0f8789636a
dhnsingh <dhnsingh@gmail.com>
2022-10-07 18:54:22 +0530

    first commit

---
+++ src/main/java/com/azure/appService/AppServiceDemoApplication.java
@@ -0,0 +1,23 @@
+package com.azure.appService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class AppServiceDemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "this is the first commit....  ! Congrats.....";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AppServiceDemoApplication.class, args);
+	}
+
+}
